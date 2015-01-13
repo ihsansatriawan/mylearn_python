d3e75a232559ac7ccfd6dfb9edfce2311400cec3
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2015-01-14 03:17:40 +0700

    add ex5.py

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'ihsan'
+my_age = 24 #comen not lie
+my_height = 170 #cm
+my_weight = 95 #kg
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair ='brown'
+
+
+print round(10.55)
+print "lets talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kg heavy." % my_weight
+print "he's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Teeth is %s." % my_teeth
+
+print "If i add %d, %d, and %d I get %d" % ( my_age, my_weight, my_weight, my_age + my_height + my_weight)
+
+
