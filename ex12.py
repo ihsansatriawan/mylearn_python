e3742c79be0b8dddfeb0d477493351ca889bbd41
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2015-01-21 07:40:49 +0700

    add ex12.py

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("Berapa umur? ")
+height = raw_input("Berapa tinggi? ")
+weight = raw_input("Berapa berat? ")
+
+print "So, you're %s old, %s tall and %s heavy." % (age, height, weight)
