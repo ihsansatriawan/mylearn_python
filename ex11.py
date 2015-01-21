6e112350350cebeda881b0e341bf3afeb6987f63
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2015-01-21 07:26:31 +0700

    add next file

---
+++ ex11.py
@@ -0,0 +1,14 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+umur = raw_input("Umur berapa? ")
+tinggi = raw_input("Tinggi berapa? ")
+berat = raw_input("Berat berapa? ")
+
+print "Jadi, kamu umur nya %s tahun, %scm dan %skg." % (umur, tinggi, berat)
